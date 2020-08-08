26fd06b187112bdf031c9c0827603c1af8756fa4
ashwini075 <ashwinigaikar075@gmail.com>
2020-08-08 19:16:02 +0530

    javascript programs

---
+++ Day4/c.js
@@ -0,0 +1,13 @@
+let counter = 1;
+
+function increment(){
+    let counterEle = document.querySelector("#counterid");
+    counter = counter + 1;
+    counterEle.innerHTML = counter;
+    }
+
+function decrement(){
+    let counterEle=document.querySelector("#counterid");
+    counter = counter - 1;
+    counterEle.innerHTML = counter;
+        }
---
+++ Day4/external.js
@@ -0,0 +1,7 @@
+function hello(){
+    alert("Congratulations for succesful completion of script !!")
+}
+
+function doit(){
+    alert(" register successfully")
+}
